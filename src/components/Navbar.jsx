5e6d25c2f0f7c481bc23c8a6c7763b77b10622b7
Atulraj0708 <atulraj07082001@gmail.com>
2023-07-28 21:21:37 +0530

    commited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
@@ -8,6 +8,7 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
+      <Typography variant="h5" component="h2" color="#FFFFFF">AR TUBE</Typography>
     </Link>
     <SearchBar />
   </Stack>
